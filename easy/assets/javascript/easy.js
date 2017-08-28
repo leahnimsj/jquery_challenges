5176d8d6ee9d1d4c12ce77f4bc69153571112b44
Leah <leah.johnson@libertymutual.com>
2017-08-28 15:50:19 -0700

    initial commit

---
+++ easy/assets/javascript/easy.js
@@ -0,0 +1,60 @@
+(function(){
+  /**
+   * Using the LocalStorage API, save the contents of the text box
+   * when users click the "save" button.
+   * Load the saved text when users click the "Load" button.
+   *
+   * USEFUL RESOURCES
+   * https://api.jquery.com/id-selector/
+   * https://api.jquery.com/val/
+   * https://api.jquery.com/click/
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+   *
+   *
+   * Here's the documentation you need:
+   * https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+   */
+
+$(function() {
+
+  var saveButton = $('#save-button');
+  var loadButton = $('#load-button');
+
+
+
+  /**
+   * Gets the text from the element for you
+   * @return {String}
+   */
+  function getText() {
+    return $('#save-me').val();
+  }
+
+  function saveText() {
+    localStorage.setItem('savedText', getText())
+    $('#save-me').val('')
+  }
+
+  $('#save-button').click(saveText);
+
+
+
+  /**
+   * Puts different text
+   * @param {String} text the stuff you want to put in the box
+   */
+  function setText(text) {
+    return $('#save-me').val(text);
+  }
+
+
+  $('#load-button').click(function(){
+
+    setText(localStorage.getItem('savedText'))
+
+  });
+
+
+})
+
+})();
